59134fc98c2e2c4ea0a0ecb8a88fafd418c006d0
muhal-git <iris.muhammet.metu@gmail.com>
2024-01-14 21:33:17 +0300

    Update main.go

--- Maps/main.go
+++ Maps/main.go
@@ -9,16 +9,30 @@ func main() {
 		"green": "#4bf745",
 	}
 
+	colors["green"] = "000000"
+	colors["green"] = "123456"
+
+	printMap(colors)
+
 	fmt.Println("colors", colors)
 
 	var colors1 map[string]string
 	fmt.Println("colors1", colors1)
 
-	colors2 := make(map[string]string, 5)
+	colors2 := make(map[string]string)
 	fmt.Println("colors2", colors2)
 	colors2["white"] = "#ffffff"
 	fmt.Println("colors2", colors2)
+	// deleting item
 	delete(colors2, "white")
 	fmt.Println("colors2", colors2)
+
+}
+
+func printMap(colors map[string]string) {
+
+	for key, value := range colors {
+		fmt.Println("Color: ", key, "Hex: ", value)
+	}
 
 }
